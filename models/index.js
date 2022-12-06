6e3935cd37092f292c74bf7070e0a761ea8fa4d1
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-12-06 15:28:28 -0600

    finished index.js in models

--- models/index.js
+++ models/index.js
@@ -5,4 +5,20 @@ const Comment = require('./Comment');
 Post.belongsTo(User, {
     foreignKey: 'userId',
     onDelete: 'CASCADE'
-  });
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'postId',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+module.exports = {
+    User,
+    Comment,
+    Post
+};
