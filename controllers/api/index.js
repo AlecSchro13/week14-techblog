c51f5dc5649fde5272d1d77d937f31a5ba5db02d
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-12-06 15:10:14 -0600

    finished exporting routes in index.js in api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,3 +4,8 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
